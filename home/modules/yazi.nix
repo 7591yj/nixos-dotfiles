257c94a18d5c69aae1fd4a24532903313b0f681d
7591yj <77034308+7591yj@users.noreply.github.com>
2026-02-13 19:13:05 +0900

    feat: setup yazi

---
+++ home/modules/yazi.nix
@@ -0,0 +1,46 @@
+{ pkgs, lib, ... }:
+
+{
+  stylix.targets.yazi.enable = true;
+
+  programs.yazi = {
+    enable = true;
+    enableBashIntegration = true;
+
+    plugins = with pkgs.yaziPlugins; {
+      inherit full-border smart-enter mount chmod lazygit;
+    };
+
+    settings = {
+      yazi = {
+        sort_dir_first = true;
+        show_hidden = true;
+        show_symlink = true;
+      };
+      keymap = {
+        mgr.prepend_keymap = [
+          { run = "plugin smart-enter"; on = "<Enter>"; }
+        ];
+      };
+      preview = {
+        image_filter = "lanczos3";
+        image_quality = 90;
+        tab_size = 1;
+        max_width = 600;
+        max_height = 900;
+        cache_dir = "";
+        ueberzug_scale = 1;
+        ueberzug_offset = [
+          0
+          0
+          0
+          0
+        ];
+      };
+    };
+
+    initLua = ''
+      require("full-border"):setup()
+    '';
+  };
+}
