a2ae097b2b3fd698e9419b4f438cf9a8e94c62be
7591yj <77034308+7591yj@users.noreply.github.com>
2026-03-05 01:15:00 +0900

    feat: add hoppscotch; update flake

--- home/modules/pwa.nix
+++ home/modules/pwa.nix
@@ -1,4 +1,13 @@
 {pkgs, ...}: {
+  xdg.desktopEntries.hoppscotch = {
+    name = "Hoppscotch";
+    exec = "helium --app=https://hoppscotch.io/";
+    icon = "${pkgs.fetchurl {
+      url = "https://avatars.githubusercontent.com/u/56705483";
+      hash = "sha256-o5TFUNSc7SpkgRWzeNcpy4bWxU1r9reDQzZ6SdWCMUg=";
+    }}";
+  };
+
   xdg.desktopEntries.plane = {
     name = "Plane";
     exec = "helium --app=https://app.plane.so/";
