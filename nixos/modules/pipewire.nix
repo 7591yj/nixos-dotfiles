c1adeb73b2f61a4187c2fb108abbce6bc77e6a31
7591yj <77034308+7591yj@users.noreply.github.com>
2026-03-07 00:33:24 +0900

    feat: update pipewire.nix to default to 96kHz on iFi dac

--- nixos/modules/pipewire.nix
+++ nixos/modules/pipewire.nix
@@ -13,6 +13,25 @@
     wireplumber.enable = true;
   };
 
+  services.pipewire.extraConfig.pipewire."10-clock-rates" = {
+    "context.properties" = {
+      "default.clock.rate" = 96000;
+      "default.clock.allowed-rates" = [44100 48000 88200 96000 176400 192000];
+    };
+  };
+
+  services.pipewire.wireplumber.extraConfig."51-ifi-node" = {
+    "monitor.alsa.rules" = [
+      {
+        matches = [{"node.description" = "iFi USB Audio SE Analog Stereo";}];
+        actions."update-props" = {
+          "audio.format" = "S32LE";
+          "audio.rate" = 96000;
+        };
+      }
+    ];
+  };
+
   services.pipewire.wireplumber.extraConfig."10-bluez-aac" = {
     "monitor.bluez.properties" = {
       "bluez5.enable-aac" = true;
