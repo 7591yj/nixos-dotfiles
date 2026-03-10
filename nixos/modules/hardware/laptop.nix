05a1331bf8dc65c755598954dbcfef819dea698f
7591yj <77034308+7591yj@users.noreply.github.com>
2026-03-11 01:00:17 +0900

    feat: add clight

--- nixos/modules/hardware/laptop.nix
+++ nixos/modules/hardware/laptop.nix
@@ -1,10 +1,19 @@
-{pkgs, ...}: {
-  services.upower.enable = true;
-
+{...}: {
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    brightnessctl
-  ];
+  services.clight = {
+    enable = true;
+    settings.backlight.disabled = true;
+    settings.kbd.disabled = true;
+    # Gamma/night-light not needed
+    settings.gamma.disabled = true;
+    settings.inhibit.inhibit_docked = true;
+  };
+
+  # geolocation required by the clight module
+  # gamma not needed; sending dummy
+  location.provider = "manual";
+  location.latitude = 0.0;
+  location.longitude = 0.0;
 }
