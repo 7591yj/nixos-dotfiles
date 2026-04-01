c62e758f928d9abf4aa7aa7cd2380e761c495420
7591yj <77034308+7591yj@users.noreply.github.com>
2026-04-02 02:53:07 +0900

    refactor: update git to use nix-wrapper-modules

--- modules/shared/programs/git.nix
+++ modules/shared/programs/git.nix
@@ -1,28 +1,42 @@
-{pkgs, ...}: {
-  programs.git = {
-    enable = true;
-    config = {
-      user = {
-        name = "7591yj";
-        email = "77034308+7591yj@users.noreply.github.com";
-      };
-      init.defaultBranch = "main";
-      core.editor = "nvim";
-      sequence.editor = "nvim";
-      alias = {
-        br = "branch";
-        ci = "commit -m ";
-        co = "checkout";
-        lg = "log --oneline --graph --decorate";
-        st = "status";
-        sw = "switch";
-      };
-      pull.rebase = true;
-      push.autoSetupRemote = true;
-    };
-  };
+{
+  pkgs,
+  inputs,
+  ...
+}:
+let
+  gitWrapper = inputs.nix-wrapper-modules.lib.evalPackage [
+    { inherit pkgs; }
+    (
+      { wlib, ... }:
+      {
+        imports = [ wlib.wrapperModules.git ];
 
+        settings = {
+          user = {
+            name = "7591yj";
+            email = "77034308+7591yj@users.noreply.github.com";
+          };
+          init.defaultBranch = "main";
+          core.editor = "nvim";
+          sequence.editor = "nvim";
+          alias = {
+            br = "branch";
+            ci = "commit -m ";
+            co = "checkout";
+            lg = "log --oneline --graph --decorate";
+            st = "status";
+            sw = "switch";
+          };
+          pull.rebase = true;
+          push.autoSetupRemote = true;
+        };
+      }
+    )
+  ];
+in
+{
   environment.systemPackages = [
+    gitWrapper
     pkgs.gh
   ];
 }
