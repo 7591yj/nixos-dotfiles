8ffab95f05c8038d314392459661a5db8a80a862
7591yj <77034308+7591yj@users.noreply.github.com>
2026-04-02 00:46:08 +0900

    refactor: delete redundant greeter definition

--- modules/nixos/desktop/compositors/default.nix
+++ modules/nixos/desktop/compositors/default.nix
@@ -1,5 +1,4 @@
 {
-  config,
   lib,
   ...
 }: {
@@ -10,14 +9,5 @@
   options.mySystem.desktop.compositor = lib.mkOption {
     type = lib.types.enum ["niri"];
     description = "Wayland compositor";
-  };
-
-  config = {
-    assertions = [
-      {
-        assertion = config.services.displayManager.dms-greeter.compositor.name == config.mySystem.desktop.compositor;
-        message = "services.displayManager.dms-greeter.compositor.name must match mySystem.desktop.compositor.";
-      }
-    ];
   };
 }
