7262bf77f368f4c9a327b9831f5eeebad95f15f9
7591yj <77034308+7591yj@users.noreply.github.com>
2026-08-23 03:25:38 +0900

    chore(pkgs): update hash for pen

--- pkgs/pen.nix
+++ pkgs/pen.nix
@@ -7,7 +7,7 @@ let
   version = "1.2.5";
   src = fetchurl {
     url = "https://www.pen.dev/download/Pen-linux-x86_64.AppImage";
-    hash = "sha256-EtLWHAKr04AQR8VC201abVj3/uwSSxEAP7/3KoYPsWA=";
+    hash = "sha256-wkiecbt6WeaUXN/1ZK3X07wGpZTEkQ1V6iBaRqDoGvo=";
   };
   contents = appimageTools.extract {
     pname = "pen";
